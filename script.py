4bcbb6694adad4461c8fb8ca916d3a71dd41a8e4
Kenny Low <kennyl41291@gmail.com>
2018-11-05 08:26:27 -0500

    Pre-deployment V1

--- script.py
+++ script.py
@@ -70,15 +70,15 @@ KennyGreatful.hasTheAppRun()
 
 #End of text application
 
-def scheduler():
-    KennyGreatful.textFunction()
+# def scheduler():
+#     KennyGreatful.textFunction()
 
-schedule.every(.15).minutes.do(scheduler)
-# schedule.every().hour.do(job)
-# schedule.every().day.at("10:30").do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every(.15).minutes.do(scheduler)
+# # schedule.every().hour.do(job)
+# # schedule.every().day.at("10:30").do(job)
+# # schedule.every().monday.do(job)
+# # schedule.every().wednesday.at("13:15").do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
